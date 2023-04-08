2b11cfe72448fcd3ff07848ef501b9430efd7b70
samabcde <tszsumng90@gmail.com>
2023-04-09 00:28:01 +0800

    complete q1861

--- src/main/kotlin/q1861/Solution.kt
+++ src/main/kotlin/q1861/Solution.kt
@@ -1,15 +1,30 @@
 package q1861
 
 class Solution {
+    companion object {
+        private const val STONE = '#'
+        private const val OBSTACLE = '*'
+        private const val SPACE = '.'
+    }
+
     fun rotateTheBox(box: Array<CharArray>): Array<CharArray> {
-        return rotate(box)
+        return box.rotate()
+            .fall()
     }
 
     // [[1, 2, 3]]-> [[1],[2],[3]]
     // 0,0->0,0
     // 0,1->1,0
     // 0,2->2,0
-    private fun rotate(box: Array<CharArray>): Array<CharArray> {
+    // [[1, 2, 3], [4, 5, 6]-> [[4, 1],[5, 2],[6, 3]]
+    // 0,0->0,1
+    // 0,1->1,1
+    // 0,2->2,1
+    // 1,0->0,0
+    // 1,1->1,0
+    // 1,2->2,0
+    private fun Array<CharArray>.rotate(): Array<CharArray> {
+        val box = this
         if (box.isEmpty()) {
             return box
         }
@@ -18,9 +33,42 @@ class Solution {
         val rotated = Array(width) { CharArray(height) }
         box.forEachIndexed { rowIndex, chars ->
             chars.forEachIndexed { colIndex, char ->
-                rotated[colIndex][rowIndex] = char
+                rotated[colIndex][height - rowIndex - 1] = char
             }
         }
-        return rotated;
+        return rotated
+    }
+
+    private fun Array<CharArray>.fall(): Array<CharArray> {
+        val box = this
+        val height = box.size
+        val width = box[0].size
+        for (col in (0 until width)) {
+            val obstacleIndexToStoneCountMap = mutableMapOf<Int, Int>()
+            var currentIndex = height
+            obstacleIndexToStoneCountMap[currentIndex] = 0
+            for (row in (0 until height).reversed()) {
+                if (box[row][col] == OBSTACLE) {
+                    currentIndex = row
+                    obstacleIndexToStoneCountMap[currentIndex] = 0
+                } else if (box[row][col] == STONE) {
+                    obstacleIndexToStoneCountMap.computeIfPresent(currentIndex) { _, v ->
+                        v + 1
+                    }
+                }
+            }
+            obstacleIndexToStoneCountMap.forEach { (obstacleIndex, stoneCount) ->
+                for (row in (obstacleIndex - stoneCount until obstacleIndex).reversed()) {
+                    box[row][col] = STONE
+                }
+                for (row in (0 until obstacleIndex - stoneCount).reversed()) {
+                    if (box[row][col] == OBSTACLE) {
+                        break
+                    }
+                    box[row][col] = SPACE
+                }
+            }
+        }
+        return box
     }
 }
--- src/test/kotlin/q1861/SolutionTest.kt
+++ src/test/kotlin/q1861/SolutionTest.kt
@@ -1,29 +1,130 @@
 package q1861
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
 class SolutionTest {
     private val solution = Solution()
 
-    @Test
-    fun rotateAllBlank() {
-        assertArrayEquals(
-            "|*, , |".toBox(), solution.rotateTheBox(
+    @TestFactory
+    fun test() = listOf(
+        Triple(
+            "obstacle 1",
+            """|*..|""",
+            """
+            |*|
+            |.|
+            |.|
+            """
+        ),
+        Triple(
+            "obstacle 2",
+            """
+                |*..|
+                |.*.|
+               """,
+            """
+                |.*|
+                |*.|
+                |..|
                 """
-                   |*|
-                   | |
-                   | |
-               """.trimIndent().toBox()
-            )
-        )
+        ),
+        Triple(
+            "obstacle 3",
+            """
+                |*..*|
+                |.**.|
+                """,
+            """
+                |.*|
+                |*.|
+                |*.|
+                |.*|
+                """
+        ),
+        Triple(
+            "stone 1",
+            "|#..|",
+            """
+            |.|
+            |.|
+            |#|
+            """
+        ),
+        Triple(
+            "stone 2",
+            """
+                |###|
+                |##.|
+               """,
+            """
+                |.#|
+                |##|
+                |##|
+                """
+        ),
+        Triple(
+            "stone 3",
+            """
+                |#..#|
+                |.##.|
+                """,
+            """
+                |..|
+                |..|
+                |##|
+                |##|
+                """
+        ),
+        Triple(
+            "stone and obstacle 1",
+            "|#*.|",
+            """
+            |#|
+            |*|
+            |.|
+            """
+        ),
+        Triple(
+            "stone and obstacle 2",
+            """
+                |#*#.|
+                |#.*#|
+               """,
+            """
+                |.#|
+                |#*|
+                |*.|
+                |##|
+                """
+        ),
+        Triple(
+            "stone and obstacle 3",
+            """
+                |#.#..***|
+                |#.*#.*#.|
+                """,
+            """
+                |..|
+                |#.|
+                |*.|
+                |.#|
+                |##|
+                |**|
+                |.*|
+                |#*|
+                """
+        ),
+    ).map { (displayName, input, expected) ->
+        DynamicTest.dynamicTest(displayName) {
+            assertArrayEquals(expected.toBox(), solution.rotateTheBox(input.toBox()))
+        }
     }
 
     private fun String.toBox(): Array<CharArray> {
-        return this.replace("|", "").split("\n").map {
-            it.split(",").map { s ->
-                s.single()
-            }.toCharArray()
+        return this.trimIndent().replace("|", "").split("\n").map { s ->
+            s.toCharArray()
         }.toTypedArray()
     }
 }
